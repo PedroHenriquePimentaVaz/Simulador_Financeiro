18ebfb0767360f2fc7b2f14df08ea0db02767350
Pedro <pedrohenriquepimentavaz@gmail.com>
2025-10-29 14:30:13 -0300

    Adicionar headers anti-cache e melhorar limpeza de service workers

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    headers: {
+      'Cache-Control': 'no-store, no-cache, must-revalidate, proxy-revalidate',
+      'Pragma': 'no-cache',
+      'Expires': '0'
+    }
+  }
 })
 
